0c96f122322b955199b6b13f94d12002559a2590
Maya <pchelaamaya@gmail.com>
2023-01-31 16:30:37 +0300

    add visual

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
+    import './style.scss'
     import Main from './components/Main/Main.vue';
+    import Filters from './components/Filters/Filters.vue'
 </script>
 
 <template>
-    <Main></Main>
+    <div class="app">
+        <Main></Main>
+        <Filters></Filters>
+    </div>
 </template>
--- src/components/Filters/Filters.vue
+++ src/components/Filters/Filters.vue
@@ -1,6 +1,45 @@
 <script setup>
-
+import './style.scss'
+import { ref } from 'vue'
+const tags = ref([
+    {
+        id: 1,
+        name: 'Сайты'
+    },
+    {
+        id: 2,
+        name: 'Верстка'
+    },
+    {
+        id: 3,
+        name: 'Тильда'
+    },
+    {
+        id: 4,
+        name: 'Очень большой текст'
+    },
+    {
+        id: 5,
+        name: 'Веб разработка'
+    },
+    {
+        id: 6,
+        name: '123'
+    },
+])
 </script>
 <template>
-
+<div class="filters">
+    <div class="filters__search">
+        <button class="search__btn">
+            <img src="@/assets/img/search.png">
+        </button>
+        <input class="search__input" type="text">
+    </div>
+    <div class="filters__tags">
+        <div class="tag" v-for="tag in tags">
+            <p class="tag__text"> {{ tag.name }}</p>
+        </div>
+    </div>
+</div>
 </template>
